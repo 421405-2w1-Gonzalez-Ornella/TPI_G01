e3df5ec6fcf6093c719e76a5b1df255bd51112e2
421405-2w1-Gonzalez-Ornella <421405@tecnicatura.frc.utn.edu.ar>
2026-09-16 01:08:22 -0300

    alta repo

---
+++ src/main/java/com/example/tpi_g01/TpiG01Application.java
@@ -0,0 +1,13 @@
+package com.example.tpi_g01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiG01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpiG01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpi_g01/TpiG01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpi_g01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiG01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
